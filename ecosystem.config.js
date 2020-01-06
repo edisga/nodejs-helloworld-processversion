f0f49a45f44e490cfaee22b39fa93971c08ea614
Edison Garcia <egachi@live.com>
2020-01-06 16:46:52 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,6 @@ module.exports = {
         env: {
             "PORT": 3000,
             "NODE_ENV": "development"
-        },
-        env_production: {
-            "PORT": 80,
-            "NODE_ENV": "production",
         }
       }
   ]
