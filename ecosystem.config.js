138a122a810d755c0e54c4ea07bebb046ef074a2
Edison Garcia <egachi@live.com>
2020-01-06 17:27:47 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,13 @@ module.exports = {
         error_file:'./error.log',
         out_file:'./output.log',
         env: {
-          "NODE_ENV": "production",
+          "NODE_ENV": "development",
           "PORT": 3000,
-        },
+        },    
+        env_production : {
+          "NODE_ENV": "production",
+          "PORT": 80,
+        }
       }
   ]
 }
