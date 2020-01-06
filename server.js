c89bb162d97608e4518d54de8dc7abfcf6d68a3d
Edison Garcia <egachi@live.com>
2020-01-06 16:41:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT;
 app.get('/', function (req, res) { res.send("NodeJS Process environment: " + process.env.NODE_ENV + " and PORT: " + process.env.PORT)});
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log('Example app listening on port:' + port));
