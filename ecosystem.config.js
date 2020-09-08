fcb3d1eb58b750c4059fc9fd340d01edd785b979
Edison Garcia <egachi@live.com>
2020-09-08 18:25:56 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "myapp1",
-        script: "./server.js",
+        script: "./custom.js",
         watch: false,
         error_file:'./error.log',
         out_file:'./output.log',
